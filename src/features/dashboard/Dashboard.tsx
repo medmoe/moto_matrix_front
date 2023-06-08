307f0e95c46256df8a1534cd2b197487236ba091
medmoe <med.seffah@gmail.com>
2023-06-08 13:24:56 -0400

    added logout endpoint

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,10 +1,30 @@
 import react, { useState } from 'react';
-
+import axios from "axios";
+import { API } from '../../types/types';
+import {useNavigate} from "react-router-dom";
 
 export function Dashboard() {
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        const options = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true,
+        }
+        await axios.post(`${API}accounts/logout/`, {}, options)
+            .then((res) => {
+                navigate("/");
+                console.log("success");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
     return (
         <div>
             <h1>Dashboard</h1>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     )
 }
